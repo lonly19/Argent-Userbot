432f8f036b977c6c61940591428a4eb1dbd7d218
Lonly <lonly@notexistcom>
2025-09-26 21:40:05 +0300

    Resolve merge conflicts

--- argent/installer/__init__.py
+++ argent/installer/__init__.py
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-﻿__all__ = [
+__all__ = [
     "create_app",
 ]
-from .app import create_app
 
-=======
-﻿__all__ = [
-    "create_app",
-]
 from .app import create_app
-
->>>>>>> 6baa4adf9f73b732ceaaaecf1d22014eadf81005
